c198800167bbb5b2050faa5a88d03fa4f61f0b5f
142011551+briansantoss@users.noreply.github.com <142011551+briansantoss@users.noreply.github.com>
2025-04-04 18:57:13 -0300

    Update: incremento dos casos de teste

--- test_sha256_hash.py
+++ test_sha256_hash.py
@@ -8,7 +8,10 @@ test_cases = [
     # Testa entradas mais longas
     ('a'* 64, 'ffe054fe7ae0cb6dc65c3af9b61d5209f439851db43d0ba5997337df154668eb'),
     ('a'*1000, '41edece42d63e8d9bf515a9ba6932e1c20cbc9f5a5d134645adb5db1b9737ea3'),
-    # Testa entrada com símbolos especiais e emojis
+    # Testa entradas semelhantes
+    ('The quick brown fox jumps over the lazy dog', 'd7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592'),
+    ('The quick brown fox jumps over the lazy dog.', 'ef537f25c895bfa782526529a9b63d97aa631564d5d789c2b765448c8635fb6c'),
+    # Testa entrada com símbolos especiais e "emojis"
     ('@#$_!', 'e3c3c475fc0c7da3e1c41df0726df5571d2cead1adc46d9fc2c4a327d39b8f4c'),
     ('🔥🚀💻', '4bb39d1c4b46ded8e30b71294605a11328500d2ac5be801e5d96b289b616b39d'),
     ('Olá, mundo!', '9583b013baa520d3a893c4270d0c67732d7ef1768eb0a13533b4e7b134d4b131'),
